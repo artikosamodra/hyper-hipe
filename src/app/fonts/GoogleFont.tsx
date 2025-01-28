3e77eda80e4034b0a6ee55f8a08197962c4d966f
artikosamodra <artikosamudra@gmail.com>
2025-01-28 16:09:32 +0700

    create google font libs

---
+++ src/app/fonts/GoogleFont.tsx
@@ -0,0 +1,11 @@
+import { Inter, Poppins } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["500", "900"],
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["500", "900"],
+});
